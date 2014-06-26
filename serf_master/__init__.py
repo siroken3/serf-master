e7769e5eb9bd76a5754da7534bb6529e2b11ce75
Kenichi Sasaki <sasaki-k@klab.com>
2014-06-26 15:03:00 +0900

    Added default handler

--- serf_master/__init__.py
+++ serf_master/__init__.py
@@ -3,14 +3,16 @@
 import os
 import logging
 
+
 class SerfHandler(object):
     def __init__(self):
         self.name = os.environ['SERF_SELF_NAME']
         _role = os.environ.get('SERF_SELF_ROLE')
         self.tag_key_value = [{'SERF_TAG_ROLE': _role}] if _role else []
+        _prefix = 'SERF_TAG_'
         self.tag_key_value = self.tag_key_value.extend(
-            [k[len('SERF_TAG_'):] + '_' + v for k, v
-             in os.environ.items() if k.startswith('SERF_TAG_')]
+            [k[len(_prefix):] + '_' + v for k, v
+             in os.environ.items() if k.startswith(_prefix)]
         )
         self.logger = logging.getLogger(type(self).__name__)
         if os.environ['SERF_EVENT'] == 'user':
@@ -22,11 +24,17 @@ class SerfHandler(object):
         self.logger.info(message)
 
 
+class _NopHandler(SerfHandler):
+    def nop(self):
+        pass
+
+
 class SerfHandlerProxy(SerfHandler):
 
-    def __init__(self):
+    def __init__(self, default=_NopHandler()):
         super(SerfHandlerProxy, self).__init__()
         self.handlers = {}
+        self.default_handler = default
 
     def register(self, tag, handler):
         if type(tag) == dict:
@@ -36,21 +44,22 @@ class SerfHandlerProxy(SerfHandler):
             # for backward compatibility
             self.handlers['ROLE_' + tag] = handler
 
-    def get_klass(self):
-        klass = False
-        for k_v in self.tag_key_value:
-            if k_v in self.handlers:
-                klass = self.handlers[k_v]
-            elif 'default' in self.handlers:
-                klass = self.handlers['default']
-        return klass
+    def get_klasses(self):
+        klasses = []
+        for k_v in self.handlers:
+            if k_v in self.tag_key_value:
+                klasses.append(self.handlers[k_v])
+            else:
+                klasses.append(self.default_handler)
+        return klasses
 
     def run(self):
-        klass = self.get_klass()
-        if not klass:
+        klasses = self.get_klasses()
+        if not klasses:
             self.log("no handler for role")
         else:
             try:
-                getattr(klass, self.event)()
+                for klass in klasses:
+                    getattr(klass, self.event, _NopHandler.nop)()
             except AttributeError:
                 self.log("event not implemented by class")
